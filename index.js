a523cd2440c66d050cc1f5f4515661a84ff22c6f
thiago sales <71419869+thiago-klairo@users.noreply.github.com>
2021-07-13 17:25:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const apiRoute = require("./api/api")
 const path = require("path")
 const cors = require("cors")
 
-
-/*ncEaminhando um arquivo html pro usuario,
-precisa primeiro dar um require("path"), para utilizar essa função/ procedimento 
-e usar o express.static, para arquivos estaticos, 
-utiliza a variável do path: path.join, para juntar duas partes de um caminho ,
-__dirname, para passar o nome do diretório */
 app.use(express.json())
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/api", apiRoute)
